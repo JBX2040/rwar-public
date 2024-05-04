71cb315adc07091d5efb6fb4f0b9df625d2487f5
JBX2040 <168923010+JBX2040@users.noreply.github.com>
2024-05-04 09:27:00 -0700

    Update Health.c

--- Server/System/Health.c
+++ Server/System/Health.c
@@ -138,6 +138,11 @@ static void damage_effect(struct rr_simulation *simulation, EntityIdx target, En
             struct rr_component_physical *physical = rr_simulation_get_physical(simulation, target);
             physical->stun_ticks = 25 * (1 + sqrtf(RR_PETAL_RARITY_SCALE[petal->rarity].heal) / 3) * (1 - physical->slow_resist);
         }
+        else if (petal->id == rr_petal_id_meat)
+        {
+            struct rr_component_physical *physical = rr_simulation_get_physical(simulation, target);
+            physical->stun_ticks = 75 * (1 + sqrtf(RR_PETAL_RARITY_SCALE[petal->rarity].heal) / 3) * (1 - physical->slow_resist);
+        }
         else if (petal->id == rr_petal_id_lightning)
         {
             lightning_petal_system(simulation, petal, target);
@@ -147,6 +152,14 @@ static void damage_effect(struct rr_simulation *simulation, EntityIdx target, En
             struct rr_component_health *health = rr_simulation_get_health(simulation, target);
             if (health->health > 0 && health->health * 2 < health->max_health)
                 rr_component_health_do_damage(simulation, health, attacker, 50 * RR_PETAL_DATA[petal->id].scale[petal->rarity].damage);
+        }
+        else if (petal->id == rr_petal_id_starfish)
+        {
+            struct rr_component_health *health = rr_simulation_get_health(simulation, target);
+            if (health->health > 0 && health->health * 2 < health->max_health)
+            rr_component_health_set_health(
+                health, health->health +
+                            0.12 * RR_PETAL_RARITY_SCALE[slot->rarity].heal);
         }
     }
 }
